d3a7b3c77ac7324b5586d718101b517c32ffc93b
John Bedingfield <bedingjd@gmail.com>
2025-07-25 10:34:38 -0500

    revise html

--- spotify-client.py
+++ spotify-client.py
@@ -31,7 +31,6 @@ app.secret_key = "aljsutnHk47slks$ksiH"         # this is used to encode the ses
 app.config['SESSION_COOKIE_NAME'] = 'spotify_track_info_app_cookie'
 # the following are for session veriables
 TOKEN_INFO = "token_info"
-REFRESH_TOKEN_INFO = "refresh_token_info"
 
 
 
@@ -58,79 +57,13 @@ def login():
 
 @app.route("/callback", methods=['GET'])
 def callback():
-    '''
-    This worked, mostly
-    code = request.args.get('code')
-    #print(f"Code is {code}")
-    res = requests.post(TOKEN_URL,
-        auth=HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET),
-        data={
-            'grant_type': 'authorization_code',
-            'code': code,
-            'redirect_uri': REDIRECT_URI
-        })
-    
-    # now that we have the access token, lets get the track info
-    this_res_json = res.json()
-    #print(f"\nres = {this_res_json}\n")
-    allTheKeys = this_res_json.keys()
-    #print(f"\nAll the keys: {allTheKeys}\n")
-    new_token = this_res_json.get('access_token')
-    #print(f"\nThe new access token is {new_token}\n")
-    refresh_token = this_res_json.get('refresh_token')
-
-    #new_token = this_res_json["access_token"]
-    #refresh_token = this_res_json['refresh_token']
-    '''
-
-    #get_current_track(new_token)
-
-    '''
-    url = "https://api.spotify.com/v1/me/player/currently-playing"
-    headers = get_auth_header(new_token)
-    payload = {
-        'grant_type': 'authorization_code',
-        'code': new_token,
-        'redirect_uri': 'http://l127.0.0.1:3000/callback',
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-    }
-    
-    response = requests.get(
-        url=url,
-        headers=headers,
-        data=payload
-    )
-    print(f"Response is {str(response)}")
-    
-
-
-    resp_json = response.json()
-    print(f"The json response is {resp_json}")
-
-    track_id = resp_json['item']['id']
-    track_name = resp_json['item']['name']
-    artists = resp_json['item']['artists']
-    artist_name = ', '.join([artist['name'] for artist in artists])
-    link = resp_json['item']['external_urls']['spotify']
-
-    current_track_info = {
-        "id": track_id,
-        "name": track_name,
-        "artists": artist_name,
-        "link": link
-    }
-    '''
-    
     sp_oauth = create_spotify_oauth()
-    session.clear()
+    session.clear()                     # clear session variables to get ready to repopulate them
     code = request.args.get('code')
     token_info = sp_oauth.get_access_token(code)
     
-
     #return json.dumps(res.json())
-    session[TOKEN_INFO] = token_info            # wasthis_res_json         # was new_token
-    #session[REFRESH_TOKEN_INFO] = refresh_token
+    session[TOKEN_INFO] = token_info    # set this session variable            # wasthis_res_json         # was new_token
     return redirect(url_for('show_track', _external=True))
 
 
@@ -151,11 +84,11 @@ def show_track():
         redirect("/login")
     sp = spotipy.Spotify(auth=token_info['access_token'])
 
-    #this_resp = sp.current_user_playing_track()
+    #this_resp = sp.current_user_playing_track()        # there are two functions that seem similar
     this_resp = sp.current_playback()
     #print(f"this response: {this_resp}")
 
-    print(f"\nThe keys: {this_resp['item'].keys()}\n")
+    #print(f"\nThe keys: {this_resp['item'].keys()}\n")
 
 
 
@@ -169,6 +102,11 @@ def show_track():
         artists = this_resp['item']['artists']
         artist_name = ', '.join([artist['name'] for artist in artists])
         link = this_resp['item']['external_urls']['spotify']
+        print(f"Link is {link}")
+        #image = this_resp['item']['images'][0]['url']
+        image = this_resp.get('item').get('album').get('images')[0].get('url')
+        #image = "test"
+        print(f"Image is {image}\n")
 
         current_track_info = {
             "id": track_id,
@@ -179,10 +117,30 @@ def show_track():
         #return current_track_info
         deets = f"Now Playing: <br>{track_name}<br>by {artist_name}"
         return f"""
-            <meta http-equiv="refresh" content="3" /> 
-            {deets}"""      # <br>The current time is {}.""".format(datetime.strftime(datetime.now(), "%d %B %Y %X"))
+            <html>
+            <meta http-equiv="refresh" content="5" />
+            <body style="background-color:#00ff00; color:white; font-family:verdana;">
+            <style type="text/css">
+                p {{margin-bottom: 5px;  margin-top: 0em;}} 
+            </style>
+            <table>
+                <tr>
+                    <td><img src="{image}" width="150" height="150"></td>
+                    <td style="padding-left: 30px;">
+                        <p style="font-size:100%;">now playing:</p>
+                        <p style="font-size:220%; test-align:center; "><b>{track_name}</b><p>
+                        <p style="font-size:150%; ">by {artist_name}</p>
+                    </td>
+                </tr>
+            </table>
+            </body></html>"""      # <br>The current time is {}.""".format(datetime.strftime(datetime.now(), "%d %B %Y %X"))
     except:
-        return "There is nothing playing at this time"
+        return f"""
+            <html>
+            <meta http-equiv="refresh" content="5" />
+            <body style="background-color:#00ff00; color:white; font-family:verdana;">
+            There is nothing playing at this time
+            </body></html>"""
     
     #return sp.current_user_saved_tracks(limit=50, offset=0)        # this worked
     return sp.current_user_playing_track()
@@ -220,18 +178,19 @@ sp.current_playback() returns the folowing:
         'type': 'playlist', 
         'uri': 'spotify:playlist:21dkzi1BzCJ3thWMKYeCFV'}, 
         'progress_ms': 305932, 
-        'item': {'album': {'album_type': 'album', 
-                'artists': [{'external_urls': {'spotify': 'https://open.spotify.com/artist/58r1rB5t3VF5X6yXGPequV'}, 'href': 'https://api.spotify.com/v1/artists/58r1rB5t3VF5X6yXGPequV', 'id': '58r1rB5t3VF5X6yXGPequV', 'name': 'Maverick City Music', 'type': 'artist', 'uri': 'spotify:artist:58r1rB5t3VF5X6yXGPequV'}, {'external_urls': {'spotify': 'https://open.spotify.com/artist/107CG0UhUl9GJnPwF83N63'}, 'href': 'https://api.spotify.com/v1/artists/107CG0UhUl9GJnPwF83N63', 'id': '107CG0UhUl9GJnPwF83N63', 'name': 'UPPERROOM', 'type': 'artist', 'uri': 'spotify:artist:107CG0UhUl9GJnPwF83N63'}], 
-                'available_markets': ['AR', 'AU', 'AT', 'BE', 'BO', 'BR', 'BG', 'CA', 'CL', 'CO', 'CR', 'CY', 'CZ', 'DK', 'DO', 'DE', 'EC', 'EE', 'SV', 'FI', 'FR', 'GR', 'GT', 'HN', 'HK', 'HU', 'IS', 'IE', 'IT', 'LV', 'LT', 'LU', 'MY', 'MT', 'MX', 'NL', 'NZ', 'NI', 'NO', 'PA', 'PY', 'PE', 'PH', 'PL', 'PT', 'SG', 'SK', 'ES', 'SE', 'CH', 'TW', 'TR', 'UY', 'US', 'GB', 'AD', 'LI', 'MC', 'ID', 'JP', 'TH', 'VN', 'RO', 'IL', 'ZA', 'SA', 'AE', 'BH', 'QA', 'OM', 'KW', 'EG', 'MA', 'DZ', 'TN', 'LB', 'JO', 'PS', 'IN', 'BY', 'KZ', 'MD', 'UA', 'AL', 'BA', 'HR', 'ME', 'MK', 'RS', 'SI', 'KR', 'BD', 'PK', 'LK', 'GH', 'KE', 'NG', 'TZ', 'UG', 'AG', 'AM', 'BS', 'BB', 'BZ', 'BT', 'BW', 'BF', 'CV', 'CW', 'DM', 'FJ', 'GM', 'GE', 'GD', 'GW', 'GY', 'HT', 'JM', 'KI', 'LS', 'LR', 'MW', 'MV', 'ML', 'MH', 'FM', 'NA', 'NR', 'NE', 'PW', 'PG', 'PR', 'WS', 'SM', 'ST', 'SN', 'SC', 'SL', 'SB', 'KN', 'LC', 'VC', 'SR', 'TL', 'TO', 'TT', 'TV', 'VU', 'AZ', 'BN', 'BI', 'KH', 'CM', 'TD', 'KM', 'GQ', 'SZ', 'GA', 'GN', 'KG', 'LA', 'MO', 'MR', 'MN', 'NP', 'RW', 'TG', 'UZ', 'ZW', 'BJ', 'MG', 'MU', 'MZ', 'AO', 'CI', 'DJ', 'ZM', 'CD', 'CG', 'IQ', 'LY', 'TJ', 'VE', 'ET', 'XK'], 
-                'external_urls': {'spotify': 'https://open.spotify.com/album/5cqTGnTR7shuPCA2FqyFtR'}, 
-                'href': 'https://api.spotify.com/v1/albums/5cqTGnTR7shuPCA2FqyFtR', 
-                'id': '5cqTGnTR7shuPCA2FqyFtR', 
-                'images': [{'height': 640, 'url': 'https://i.scdn.co/image/ab67616d0000b273ad29a40953b33e52530e399e', 'width': 640}, {'height': 300, 'url': 'https://i.scdn.co/image/ab67616d00001e02ad29a40953b33e52530e399e', 'width': 300}, {'height': 64, 'url': 'https://i.scdn.co/image/ab67616d00004851ad29a40953b33e52530e399e', 'width': 64}], 
-                'name': 'Move Your Heart', 
-                'release_date': '2021-01-29', 
-                'release_date_precision': 'day', 
-                'total_tracks': 7, 
-                'type': 'album', 
-                'uri': 'spotify:album:5cqTGnTR7shuPCA2FqyFtR'}, 
-                'artists': [{'external_urls': {'spotify': 'https://open.spotify.com/artist/58r1rB5t3VF5X6yXGPequV'}, 'href': 'https://api.spotify.com/v1/artists/58r1rB5t3VF5X6yXGPequV', 'id': '58r1rB5t3VF5X6yXGPequV', 'name': 'Maverick City Music', 'type': 'artist', 'uri': 'spotify:artist:58r1rB5t3VF5X6yXGPequV'}, {'external_urls': {'spotify': 'https://open.spotify.com/artist/107CG0UhUl9GJnPwF83N63'}, 'href': 'https://api.spotify.com/v1/artists/107CG0UhUl9GJnPwF83N63', 'id': '107CG0UhUl9GJnPwF83N63', 'name': 'UPPERROOM', 'type': 'artist', 'uri': 'spotify:artist:107CG0UhUl9GJnPwF83N63'}, {'external_urls': {'spotify': 'https://open.spotify.com/artist/1bdnGJxkbIIys5Jhk1T74v'}, 'href': 'https://api.spotify.com/v1/artists/1bdnGJxkbIIys5Jhk1T74v', 'id': '1bdnGJxkbIIys5Jhk1T74v', 'name': 'Brandon Lake', 'type': 'artist', 'uri': 'spotify:artist:1bdnGJxkbIIys5Jhk1T74v'}, {'external_urls': {'spotify': 'https://open.spotify.com/artist/0vUp0HrA2d7mcExuf5Wbo6'}, 'href': 'https://api.spotify.com/v1/artists/0vUp0HrA2d7mcExuf5Wbo6', 'id': '0vUp0HrA2d7mcExuf5Wbo6', 'name': 'Eniola Abioye', 'type': 'artist', 'uri': 'spotify:artist:0vUp0HrA2d7mcExuf5Wbo6'}], 'available_markets': ['AR', 'AU', 'AT', 'BE', 'BO', 'BR', 'BG', 'CA', 'CL', 'CO', 'CR', 'CY', 'CZ', 'DK', 'DO', 'DE', 'EC', 'EE', 'SV', 'FI', 'FR', 'GR', 'GT', 'HN', 'HK', 'HU', 'IS', 'IE', 'IT', 'LV', 'LT', 'LU', 'MY', 'MT', 'MX', 'NL', 'NZ', 'NI', 'NO', 'PA', 'PY', 'PE', 'PH', 'PL', 'PT', 'SG', 'SK', 'ES', 'SE', 'CH', 'TW', 'TR', 'UY', 'US', 'GB', 'AD', 'LI', 'MC', 'ID', 'JP', 'TH', 'VN', 'RO', 'IL', 'ZA', 'SA', 'AE', 'BH', 'QA', 'OM', 'KW', 'EG', 'MA', 'DZ', 'TN', 'LB', 'JO', 'PS', 'IN', 'BY', 'KZ', 'MD', 'UA', 'AL', 'BA', 'HR', 'ME', 'MK', 'RS', 'SI', 'KR', 'BD', 'PK', 'LK', 'GH', 'KE', 'NG', 'TZ', 'UG', 'AG', 'AM', 'BS', 'BB', 'BZ', 'BT', 'BW', 'BF', 'CV', 'CW', 'DM', 'FJ', 'GM', 'GE', 'GD', 'GW', 'GY', 'HT', 'JM', 'KI', 'LS', 'LR', 'MW', 'MV', 'ML', 'MH', 'FM', 'NA', 'NR', 'NE', 'PW', 'PG', 'PR', 'WS', 'SM', 'ST', 'SN', 'SC', 'SL', 'SB', 'KN', 'LC', 'VC', 'SR', 'TL', 'TO', 'TT', 'TV', 'VU', 'AZ', 'BN', 'BI', 'KH', 'CM', 'TD', 'KM', 'GQ', 'SZ', 'GA', 'GN', 'KG', 'LA', 'MO', 'MR', 'MN', 'NP', 'RW', 'TG', 'UZ', 'ZW', 'BJ', 'MG', 'MU', 'MZ', 'AO', 'CI', 'DJ', 'ZM', 'CD', 'CG', 'IQ', 'LY', 'TJ', 'VE', 'ET', 'XK'], 'disc_number': 1, 'duration_ms': 337152, 'explicit': False, 'external_ids': {'isrc': 'TCAFI2167747'}, 'external_urls': {'spotify': 'https://open.spotify.com/track/0A402ZdxwQzaVdyH5Zav5X'}, 'href': 'https://api.spotify.com/v1/tracks/0A402ZdxwQzaVdyH5Zav5X', 'id': '0A402ZdxwQzaVdyH5Zav5X', 'is_local': False, 'name': 'Rest on Us (feat. Brandon Lake & Eniola Abioye)', 'popularity': 72, 'preview_url': None, 'track_number': 1, 'type': 'track', 'uri': 'spotify:track:0A402ZdxwQzaVdyH5Zav5X'}, 'currently_playing_type': 'track', 'actions': {'disallows': {'resuming': True}}}
+        'item': {'album': 
+                    {'album_type': 'album', 
+                    'artists': [{'external_urls': {'spotify': 'https://open.spotify.com/artist/58r1rB5t3VF5X6yXGPequV'}, 'href': 'https://api.spotify.com/v1/artists/58r1rB5t3VF5X6yXGPequV', 'id': '58r1rB5t3VF5X6yXGPequV', 'name': 'Maverick City Music', 'type': 'artist', 'uri': 'spotify:artist:58r1rB5t3VF5X6yXGPequV'}, {'external_urls': {'spotify': 'https://open.spotify.com/artist/107CG0UhUl9GJnPwF83N63'}, 'href': 'https://api.spotify.com/v1/artists/107CG0UhUl9GJnPwF83N63', 'id': '107CG0UhUl9GJnPwF83N63', 'name': 'UPPERROOM', 'type': 'artist', 'uri': 'spotify:artist:107CG0UhUl9GJnPwF83N63'}], 
+                    'available_markets': ['AR', 'AU', 'AT', 'BE', 'BO', 'BR', 'BG', 'CA', 'CL', 'CO', 'CR', 'CY', 'CZ', 'DK', 'DO', 'DE', 'EC', 'EE', 'SV', 'FI', 'FR', 'GR', 'GT', 'HN', 'HK', 'HU', 'IS', 'IE', 'IT', 'LV', 'LT', 'LU', 'MY', 'MT', 'MX', 'NL', 'NZ', 'NI', 'NO', 'PA', 'PY', 'PE', 'PH', 'PL', 'PT', 'SG', 'SK', 'ES', 'SE', 'CH', 'TW', 'TR', 'UY', 'US', 'GB', 'AD', 'LI', 'MC', 'ID', 'JP', 'TH', 'VN', 'RO', 'IL', 'ZA', 'SA', 'AE', 'BH', 'QA', 'OM', 'KW', 'EG', 'MA', 'DZ', 'TN', 'LB', 'JO', 'PS', 'IN', 'BY', 'KZ', 'MD', 'UA', 'AL', 'BA', 'HR', 'ME', 'MK', 'RS', 'SI', 'KR', 'BD', 'PK', 'LK', 'GH', 'KE', 'NG', 'TZ', 'UG', 'AG', 'AM', 'BS', 'BB', 'BZ', 'BT', 'BW', 'BF', 'CV', 'CW', 'DM', 'FJ', 'GM', 'GE', 'GD', 'GW', 'GY', 'HT', 'JM', 'KI', 'LS', 'LR', 'MW', 'MV', 'ML', 'MH', 'FM', 'NA', 'NR', 'NE', 'PW', 'PG', 'PR', 'WS', 'SM', 'ST', 'SN', 'SC', 'SL', 'SB', 'KN', 'LC', 'VC', 'SR', 'TL', 'TO', 'TT', 'TV', 'VU', 'AZ', 'BN', 'BI', 'KH', 'CM', 'TD', 'KM', 'GQ', 'SZ', 'GA', 'GN', 'KG', 'LA', 'MO', 'MR', 'MN', 'NP', 'RW', 'TG', 'UZ', 'ZW', 'BJ', 'MG', 'MU', 'MZ', 'AO', 'CI', 'DJ', 'ZM', 'CD', 'CG', 'IQ', 'LY', 'TJ', 'VE', 'ET', 'XK'], 
+                    'external_urls': {'spotify': 'https://open.spotify.com/album/5cqTGnTR7shuPCA2FqyFtR'}, 
+                    'href': 'https://api.spotify.com/v1/albums/5cqTGnTR7shuPCA2FqyFtR', 
+                    'id': '5cqTGnTR7shuPCA2FqyFtR', 
+                    'images': [{'height': 640, 'url': 'https://i.scdn.co/image/ab67616d0000b273ad29a40953b33e52530e399e', 'width': 640}, {'height': 300, 'url': 'https://i.scdn.co/image/ab67616d00001e02ad29a40953b33e52530e399e', 'width': 300}, {'height': 64, 'url': 'https://i.scdn.co/image/ab67616d00004851ad29a40953b33e52530e399e', 'width': 64}], 
+                    'name': 'Move Your Heart', 
+                    'release_date': '2021-01-29', 
+                    'release_date_precision': 'day', 
+                    'total_tracks': 7, 
+                    'type': 'album', 
+                    'uri': 'spotify:album:5cqTGnTR7shuPCA2FqyFtR'}, 
+                    'artists': [{'external_urls': {'spotify': 'https://open.spotify.com/artist/58r1rB5t3VF5X6yXGPequV'}, 'href': 'https://api.spotify.com/v1/artists/58r1rB5t3VF5X6yXGPequV', 'id': '58r1rB5t3VF5X6yXGPequV', 'name': 'Maverick City Music', 'type': 'artist', 'uri': 'spotify:artist:58r1rB5t3VF5X6yXGPequV'}, {'external_urls': {'spotify': 'https://open.spotify.com/artist/107CG0UhUl9GJnPwF83N63'}, 'href': 'https://api.spotify.com/v1/artists/107CG0UhUl9GJnPwF83N63', 'id': '107CG0UhUl9GJnPwF83N63', 'name': 'UPPERROOM', 'type': 'artist', 'uri': 'spotify:artist:107CG0UhUl9GJnPwF83N63'}, {'external_urls': {'spotify': 'https://open.spotify.com/artist/1bdnGJxkbIIys5Jhk1T74v'}, 'href': 'https://api.spotify.com/v1/artists/1bdnGJxkbIIys5Jhk1T74v', 'id': '1bdnGJxkbIIys5Jhk1T74v', 'name': 'Brandon Lake', 'type': 'artist', 'uri': 'spotify:artist:1bdnGJxkbIIys5Jhk1T74v'}, {'external_urls': {'spotify': 'https://open.spotify.com/artist/0vUp0HrA2d7mcExuf5Wbo6'}, 'href': 'https://api.spotify.com/v1/artists/0vUp0HrA2d7mcExuf5Wbo6', 'id': '0vUp0HrA2d7mcExuf5Wbo6', 'name': 'Eniola Abioye', 'type': 'artist', 'uri': 'spotify:artist:0vUp0HrA2d7mcExuf5Wbo6'}], 'available_markets': ['AR', 'AU', 'AT', 'BE', 'BO', 'BR', 'BG', 'CA', 'CL', 'CO', 'CR', 'CY', 'CZ', 'DK', 'DO', 'DE', 'EC', 'EE', 'SV', 'FI', 'FR', 'GR', 'GT', 'HN', 'HK', 'HU', 'IS', 'IE', 'IT', 'LV', 'LT', 'LU', 'MY', 'MT', 'MX', 'NL', 'NZ', 'NI', 'NO', 'PA', 'PY', 'PE', 'PH', 'PL', 'PT', 'SG', 'SK', 'ES', 'SE', 'CH', 'TW', 'TR', 'UY', 'US', 'GB', 'AD', 'LI', 'MC', 'ID', 'JP', 'TH', 'VN', 'RO', 'IL', 'ZA', 'SA', 'AE', 'BH', 'QA', 'OM', 'KW', 'EG', 'MA', 'DZ', 'TN', 'LB', 'JO', 'PS', 'IN', 'BY', 'KZ', 'MD', 'UA', 'AL', 'BA', 'HR', 'ME', 'MK', 'RS', 'SI', 'KR', 'BD', 'PK', 'LK', 'GH', 'KE', 'NG', 'TZ', 'UG', 'AG', 'AM', 'BS', 'BB', 'BZ', 'BT', 'BW', 'BF', 'CV', 'CW', 'DM', 'FJ', 'GM', 'GE', 'GD', 'GW', 'GY', 'HT', 'JM', 'KI', 'LS', 'LR', 'MW', 'MV', 'ML', 'MH', 'FM', 'NA', 'NR', 'NE', 'PW', 'PG', 'PR', 'WS', 'SM', 'ST', 'SN', 'SC', 'SL', 'SB', 'KN', 'LC', 'VC', 'SR', 'TL', 'TO', 'TT', 'TV', 'VU', 'AZ', 'BN', 'BI', 'KH', 'CM', 'TD', 'KM', 'GQ', 'SZ', 'GA', 'GN', 'KG', 'LA', 'MO', 'MR', 'MN', 'NP', 'RW', 'TG', 'UZ', 'ZW', 'BJ', 'MG', 'MU', 'MZ', 'AO', 'CI', 'DJ', 'ZM', 'CD', 'CG', 'IQ', 'LY', 'TJ', 'VE', 'ET', 'XK'], 'disc_number': 1, 'duration_ms': 337152, 'explicit': False, 'external_ids': {'isrc': 'TCAFI2167747'}, 'external_urls': {'spotify': 'https://open.spotify.com/track/0A402ZdxwQzaVdyH5Zav5X'}, 'href': 'https://api.spotify.com/v1/tracks/0A402ZdxwQzaVdyH5Zav5X', 'id': '0A402ZdxwQzaVdyH5Zav5X', 'is_local': False, 'name': 'Rest on Us (feat. Brandon Lake & Eniola Abioye)', 'popularity': 72, 'preview_url': None, 'track_number': 1, 'type': 'track', 'uri': 'spotify:track:0A402ZdxwQzaVdyH5Zav5X'}, 'currently_playing_type': 'track', 'actions': {'disallows': {'resuming': True}}}
 '''
